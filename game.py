2cbe77d79693721652bd59978b67d2b3afccadba
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2023-08-01 00:19:35 +0300

    inithial_commit

---
+++ game.py
@@ -0,0 +1,34 @@
+import random
+my_num = random.randint(1,100)
+print(my_num)
+#print('Привет, поиграем в угадай число')
+#print('Я загадаю число от 1 до 100 попробуй его угадать')
+#user_num = input('Введите число от 1 до 100  ')
+
+#def is_valid(user_num):
+    #return  user_num.isdigit () and 1 <= int(user_num) <= 100
+
+
+
+#def game(user_num):
+    #while True:
+        #if  not is_valid(user_num):
+
+            #print('А может все таки число от 1 го до 100')
+        
+        #else:
+            #if user_num > my_num:
+                #print('Я згадал число меньше твоего')
+            #if user_num < my_num:
+                #print('Я загадал число больше твоего')
+
+            #if user_num == my_num:
+                #print("Ты выйграл, сыграем еще?")
+            #break
+        
+            
+#print('Спасибо что поиграли со мной еще увидимся')
+        
+    
+        
+
