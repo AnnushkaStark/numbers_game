d5dfb75d418889c28a60071833e0d942282029ef
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2023-08-05 01:02:34 +0300

    test_bot_create

--- game.py
+++ game.py
@@ -1,34 +1,27 @@
+import telebot 
 import random
-my_num = random.randint(1,100)
-print(my_num)
-#print('Привет, поиграем в угадай число')
-#print('Я загадаю число от 1 до 100 попробуй его угадать')
-#user_num = input('Введите число от 1 до 100  ')
-
-#def is_valid(user_num):
-    #return  user_num.isdigit () and 1 <= int(user_num) <= 100
-
-
-
-#def game(user_num):
-    #while True:
-        #if  not is_valid(user_num):
-
-            #print('А может все таки число от 1 го до 100')
-        
-        #else:
-            #if user_num > my_num:
-                #print('Я згадал число меньше твоего')
-            #if user_num < my_num:
-                #print('Я загадал число больше твоего')
-
-            #if user_num == my_num:
-                #print("Ты выйграл, сыграем еще?")
-            #break
-        
-            
-#print('Спасибо что поиграли со мной еще увидимся')
-        
+bot = telebot.TeleBot('6029515865:AAGYMP9S2QjY3e69ekjkHClJnOmziEhMiTY')
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id,'Привет, поиграем в угадай число')
+    bot.send_message(message.chat.id,'Я загадаю число от 1 до 100 попробуй его угадать')
+    bot.send_message(message.chat.id,'Введите число от 1 до 100')
     
-        
+@bot.message_handler(func= lambda message:True)
+def get_text_message(message):
+    bot_num  = random.randint(1,100)
+    user_message = message.text.lower()
+    user_num = int(user_message)
+   
+    if 1 > user_num > 100:
+        bot.send_message(message.chat.id,'А может все таки число от 1 го до 100')
+    elif user_num < bot_num:
+        bot.send_message(message.chat.id,'Я загадал число больше твоего')
+    elif user_num > bot_num:
+        bot.send_message(message.chat.id,'Я загадал число меньше твоего')
+    elif user_num == bot_num:
+        bot.send_message(message.chat.id,'Ты выйграл, сыграем еще?')
+
+bot.polling()
+
 
