3fb149b0a7db97137df13f1a677e6c692f9bf4f9
AnnushkaStark <130693211+AnnushkaStark@users.noreply.github.com>
2023-08-10 01:28:15 +0300

    fix bugs

--- game.py
+++ game.py
@@ -6,20 +6,21 @@ def start(message):
     bot.send_message(message.chat.id,'Привет, поиграем в угадай число')
     bot.send_message(message.chat.id,'Я загадаю число от 1 до 100 попробуй его угадать')
     bot.send_message(message.chat.id,'Введите число от 1 до 100')
-    
+
+
+bot_num  = random.randint(1,100)    
 @bot.message_handler(func= lambda message:True)
 def get_text_message(message):
-    bot_num  = random.randint(1,100)
     user_message = message.text.lower()
-    user_num = int(user_message)
-   
-    if 1 > user_num > 100:
+    if  user_message.isalpha():
         bot.send_message(message.chat.id,'А может все таки число от 1 го до 100')
-    elif user_num < bot_num:
+    if user_message.isdigit() and 1 > int(user_message) > 100:
+        bot.send_message(message.chat.id,'А может все таки число от 1 го до 100')
+    if user_message.isdigit() and  int(user_message) < bot_num:
         bot.send_message(message.chat.id,'Я загадал число больше твоего')
-    elif user_num > bot_num:
+    if  user_message.isdigit() and  int(user_message) > bot_num:
         bot.send_message(message.chat.id,'Я загадал число меньше твоего')
-    elif user_num == bot_num:
+    if  user_message.isdigit() and  int(user_message) == bot_num:
         bot.send_message(message.chat.id,'Ты выйграл, сыграем еще?')
 
 bot.polling()
